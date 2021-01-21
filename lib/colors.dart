32b6264bb2f9f4e7ae454010ed0a727d42a39268
Mustafa <18701031@mersin.edu.tr>
2021-01-21 23:55:38 +0300

    Color Added

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 Color redcolor = new Color(0xFFF86B68);
+Color redTransColor = new Color(0xFFFAE7E7);
+Color blueColor = new Color(0xFF4F62BF);
+Color blueTransColor = new Color(0xFFD9E1FF);
